076f4381b5f0c8ec4450b35cc609ed2963ce4bcd
mateusPPinh <rawrenete@gmail.com>
2024-03-17 20:40:25 -0300

    env

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `window.allowedOrigin = '${process.env.NEXT_PUBLIC_UMBRIEL_FRONT_LOCAL_ADDRRESS}';`,
+        }}
+      />
       </body>
     </Html>
   );
